c999a6f4597c9e2fa030246e06ec002095565eab
RoystonDAlmeida <roystonjan1@hotmail.com>
2024-09-20 14:47:00 +0530

    Day 1:Designed partial interface

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 className="todo-h1">Simple To-do app</h1>
       </header>
+      <body>
+        <br></br>
+        <form action="/submit_form" method="post">
+          <div className='heading'>
+            <label for="heading">Heading:</label>
+            <input type="text" id="heading" name="heading" placeholder='Type the main heading ot the task' required></input>
+          </div>
+        </form>
+      </body>
     </div>
   );
 }
